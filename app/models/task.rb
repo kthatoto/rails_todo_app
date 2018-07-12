14784f954f0e1a3e58f09559ea678efe582ff0dd
kthatoto <kthatoto@gmail.com>
2018-07-13 08:33:44 +0900

    Added validation to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
   scope :recently_updated, -> { order(updated_at: :desc) }
   enum status: {todo: 0, done: 1}
+
+  validates :assignee, presence: true
+  validates :content,  presence: true
+  validates :status,   presence: true
 end
