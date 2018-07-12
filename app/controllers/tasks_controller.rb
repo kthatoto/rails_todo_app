4c03ab32392f034f4b95ede19746b4ed3043010c
kthatoto <kthatoto@gmail.com>
2018-07-13 07:15:20 +0900

    Added tasks list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,9 @@ class TasksController < ApplicationController
 
   def index
     @new_task = Task.new
+    @all_tasks = Taks.all
+    @todo_tasks = @all_tasks.where(status: :todo)
+    @done_tasks = @all_tasks.where(status: :done)
   end
   def create
   end
