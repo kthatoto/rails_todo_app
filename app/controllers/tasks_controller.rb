a69fa024f90cd18e0054b7561267c500007ed334
kthatoto <kthatoto@gmail.com>
2018-07-13 08:23:44 +0900

    Update complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
 
   def index
     @new_task = Task.new
-    @all_tasks = Task.all
+    @all_tasks = Task.all.recently_updated
     @todo_tasks = @all_tasks.where(status: :todo)
     @done_tasks = @all_tasks.where(status: :done)
   end
@@ -11,5 +11,17 @@ class TasksController < ApplicationController
   def edit
   end
   def update
+    @task = Task.find_by(id: params[:id].to_i)
+    unless @task
+      render :root
+    end
+    if @task.update(task_params)
+      redirect_to :root
+    end
   end
+  private
+    def task_params
+      params[:task][:status] = params[:task][:status].to_i if params[:task] && params[:task][:status]
+      params.require(:task).permit(:assignee, :content, :status)
+    end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
+  scope :recently_updated, -> { order(updated_at: :desc) }
   enum status: {todo: 0, done: 1}
 end
